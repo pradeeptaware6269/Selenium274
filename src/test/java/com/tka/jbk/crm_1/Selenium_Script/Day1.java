2ae9f1df7be78c3fa71a9b36e4946ac742de51f5
pradeeptaware6269 <pradeeptaware6269@gmail.com>
2025-12-13 11:25:18 +0530

    First Commit

---
+++ src/test/java/com/tka/jbk/crm_1/Selenium_Script/Day1.java
@@ -0,0 +1,23 @@
+package com.tka.jbk.crm_1.Selenium_Script;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Day1 {
+	public static void main(String[] args) {
+		//open chrome browser
+		//ChromeDriver driver=new ChromeDriver();
+		
+		//fire fox browser
+		//FirefoxDriver driver=new FirefoxDriver();
+		
+		//EdgeDriver driver=new EdgeDriver();
+		
+		
+		int[] arr=new int[9];
+		System.out.println(arr.length-1);
+	}
+
+}
