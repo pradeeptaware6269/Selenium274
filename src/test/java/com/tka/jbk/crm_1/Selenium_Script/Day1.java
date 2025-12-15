1eafb4befbd6b9deced9025e46efe4066a5d8fd0
pradeeptaware6269 <pradeeptaware6269@gmail.com>
2025-12-15 18:19:45 +0530

    add Day2 class in a package

--- src/test/java/com/tka/jbk/crm_1/Selenium_Script/Day1.java
+++ src/test/java/com/tka/jbk/crm_1/Selenium_Script/Day1.java
@@ -1,5 +1,7 @@
 package com.tka.jbk.crm_1.Selenium_Script;
 
+import java.sql.Driver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,17 +9,15 @@ import org.openqa.selenium.safari.SafariDriver;
 
 public class Day1 {
 	public static void main(String[] args) {
+		System.out.println("Welcome to selenim module");
 		//open chrome browser
 		//ChromeDriver driver=new ChromeDriver();
 		
 		//fire fox browser
 		//FirefoxDriver driver=new FirefoxDriver();
 		
-		//EdgeDriver driver=new EdgeDriver();
-		
-		
-		int[] arr=new int[9];
-		System.out.println(arr.length-1);
+		EdgeDriver driver=new EdgeDriver();
+		driver.get("https://github.com/pradeeptaware6269/Selenium274/blob/main/src/test/java/com/tka/jbk/crm_1/Selenium_Script/Day1.javal");
 	}
 
 }
---
+++ src/test/java/com/tka/jbk/crm_1/Selenium_Script/Day2.java
@@ -0,0 +1,12 @@
+package com.tka.jbk.crm_1.Selenium_Script;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day2 {
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get(
+				"https://github.com/pradeeptaware6269/Selenium274/blob/main/src/test/java/com/tka/jbk/crm_1/Selenium_Script/Day1.java");
+	}
+
+}
