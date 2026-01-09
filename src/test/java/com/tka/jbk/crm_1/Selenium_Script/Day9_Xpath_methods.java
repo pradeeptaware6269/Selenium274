2e15aa3f488df5a90e9a4df03c0239085390e65b
pradeeptaware6269 <pradeeptaware6269@gmail.com>
2026-01-09 10:35:44 +0530

    xpath methods

---
+++ src/test/java/com/tka/jbk/crm_1/Selenium_Script/Day9_Xpath_methods.java
@@ -0,0 +1,43 @@
+package com.tka.jbk.crm_1.Selenium_Script;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day9_Xpath_methods {
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+
+		// Relative XPath
+		WebElement firstname = driver.findElement(By.xpath("//input[@id='name']"));
+		firstname.sendKeys("Pradeep");
+
+		// using contains()
+		WebElement email = driver.findElement(By.xpath("//input[contains(@id,'email')]"));
+		email.sendKeys("pradeep@gmail.com");
+
+		// using * instead of tagname
+		WebElement phone = driver.findElement(By.xpath("//*[@id='phone']"));
+		phone.sendKeys("8454216562");
+
+		// using starts-with()
+		WebElement address = driver.findElement(By.xpath("//textarea[starts-with(@id,'textarea')]"));
+		address.sendKeys("Baramati Pune");
+
+		// ends-with() is NOT supported in Selenium XPath 1.0
+
+		// using multiple attributes with AND
+		WebElement search = driver.findElement(
+				By.xpath("//input[@class='wikipedia-search-input' and @id='Wikipedia1_wikipedia-search-input']"));
+		search.sendKeys("Selenium");
+
+		// using multiple attributes with OR (CORRECTED)
+		WebElement searchButton = driver
+				.findElement(By.xpath("//input[@class='wikipedia-search-button' or @type='submit']"));
+		searchButton.click();
+	}
+}
